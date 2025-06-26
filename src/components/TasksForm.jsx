c458435973c93e4061200ff13d050e6a0cdb1ee6
londonohidalgo@gmail.com <londonohidalgo@gmail.com>
2025-06-26 10:42:34 +0200

    taskForm done

--- src/components/TasksForm.jsx
+++ src/components/TasksForm.jsx
@@ -1,33 +1,64 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
+const TasksForm = ({ categories, createTask }) => {
+  const [newTask, setNewTask] = useState({ description: "", category: "" });
 
-const TasksForm = ({categories, createTask, isLoading})=>{
-  const [newTask, setNewTask]=useState("");
-  const [catTask, setCatTask]= useState("");
+  const handleSubmitTask = (e) => {
+    e.preventDefault();
+    if (!newTask.description.trim() || !newTask.category) {
+      alert("Veuillez rentrer une tâche et choisir une catégorie");
+      return;
+    }
 
-  const handleSubmitTask =()=>{
     createTask(newTask);
-  }
+    setNewTask({ description: "", category: "" });
+  };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewTask((prevTask) => ({
+      ...prevTask,
+      [name]: value,
+    }));
+  };
 
-    return (
-      <form onSubmit={handleSubmitTask} className="flex flex-row justify-center m-4 pl-4">
-        <input
-          type="text"
-          value={newTask}
-          onChange={(e)=>setNewTask(e.target.value)}
-          placeholder="Nouvelle tâche"
-          className="border rounded-lg border-gray-600 bg-white  p-2 mx-4 min-w-[335px]"
-        />
-        <select className="border rounded-lg border-gray-600 bg-white p-2 ">
-          <option>Toutes les categories</option>
-        </select>
-        <button className="bg-indigo-500 text-zinc-100 font-bold p-2 rounded-lg mx-4 min-w-[160px]">
-          Ajouter une tâche
-        </button>
-      </form>
+  return (
+    <form
+      onSubmit={handleSubmitTask}
+      className="flex flex-row justify-center m-4 pl-4"
+    >
+      <input
+        type="text"
+        name="description"
+        value={newTask.description}
+        onChange={handleChange}
+        placeholder="Nouvelle tâche"
+        className="border rounded-lg border-gray-600 bg-white  p-2 mx-4 min-w-[335px]"
+      />
+      <select
+        onChange={handleChange}
+        className="border rounded-lg border-gray-600 bg-white p-2 "
+      >
+        <option value="" disabled>
+          Choisir une catégorie
+        </option>
+        {categories &&
+          categories.map((cat) => {
+            return (
+              <option key={cat.i} value={cat.id}>
+                {cat.name}
+              </option>
+            );
+          })}
+      </select>
+      <button
+        type="submit"
+        className="bg-indigo-500 text-zinc-100 font-bold p-2 rounded-lg mx-4 min-w-[160px]"
+      >
+        Ajouter une tâche
+      </button>
+    </form>
+  );
+};
 
-    )
-}
-
-export default TasksForm; 
+export default TasksForm;
